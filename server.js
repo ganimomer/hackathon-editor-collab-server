2cefda386c2d51ea8c620ba1e14b53c2df7ed278
DaQuirm <leo.daquirm@gmail.com>
2015-12-16 11:24:40 +0200

    Get rid of closure userId and siteId

--- server.js
+++ server.js
@@ -4,20 +4,24 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const sessions = new Map();
+const sessions = new Map()
+const users = new Map()
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/mock-client.html')
 })
 
+const forAllRooms = (socket, action) => {
+  socket.rooms
+    .filter(room => room !== socket.id)
+    .forEach(action)
+}
+
 io.on('connection', (socket) => {
+  console.log('connected')
 
-  let userId
-  let siteId
-
-  console.log('connected');
-  socket.on('join', (data) => {
-  	userId = data.userId
-  	siteId = data.siteId
+  socket.on('join', data => {
+    users.set(socket.id, data.userId)
     socket.join(data.siteId)
     const session = sessions.get(data.siteId)
     if (session) {
@@ -33,29 +37,33 @@ io.on('connection', (socket) => {
     socket.broadcast.to(data.siteId).emit('enter', data)
   })
 
-  socket.on('message', (data) => {
+  socket.on('message', data => {
     console.log(`message: ${JSON.stringify(data)}`)
-    data.userId = userId
-    socket.broadcast.to(siteId).emit('message', data)
+    data.userId = users.get(socket.id)
+    forAllRooms(socket, room => socket.broadcast.to(room).emit('message', data))
   })
 
   socket.on('disconnect', () => {
+    const userId = users.get(socket.id)
     console.log(`${userId} has disconnected`)
-    const session = sessions.get(siteId)
-    if (userId === session.editing) {
-      const newEditor = session.viewing.shift()
-      if (newEditor) {
-        session.editing = newEditor
-        io.to(siteId).emit('leave', { userId, newEditor })
-        console.log(`${userId} was king but now ${newEditor} was crowned`)
+
+    forAllRooms(socket, room => {
+      const session = sessions.get(room)
+      if (userId === session.editing) {
+        const newEditor = session.viewing.shift()
+        if (newEditor) {
+          session.editing = newEditor
+          io.to(room).emit('leave', { userId, newEditor })
+          console.log(`${userId} was king but now ${newEditor} was crowned`)
+        } else {
+          sessions.delete(room)
+          console.log(`${userId} was king but now the kingdom is gone`)
+        }
       } else {
-        sessions.delete(siteId)
-        console.log(`${userId} was king but now the kingdom is gone`)
+        session.viewing.splice(session.viewing.indexOf(userId), 1)
+        io.to(room).emit('leave', { userId })
       }
-    } else {
-      session.viewing.splice(session.viewing.indexOf(userId), 1)
-      io.to(siteId).emit('leave', { userId })
-    }
+    })
   })
 })
 
